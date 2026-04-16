14c5cddf8077e7438a0fb0f936dc1765dc56f455
michaelciudad <mi.ciudad@duocuc.cl>
2026-04-16 13:21:27 -0400

    Add files via upload
    En este commit, realizamos la conexion del properties al sql, faltan los atributos en el heidi para la base de datos, creamos las carpetas bases para el proyecto, faltando los atributos que usaremos para la app

---
+++ fluxpuzzle/fluxpuzzle/src/main/java/cl/duoc/fluxpuzzle/FluxpuzzleApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.fluxpuzzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxpuzzleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxpuzzleApplication.class, args);
+	}
+
+}
